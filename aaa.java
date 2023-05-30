d513aefd559cd34780e9ea1264c34e18aea5fe3f
totalkat <134828152+totalkat@users.noreply.github.com>
2023-05-29 22:10:19 -0700

    update stamp.java

--- aaa.java
+++ aaa.java
@@ -1,7 +1,7 @@
 
 import java.io.*;
 import java.util.*;
-public class aaa{
+public class stamp{
     public static void main(String[] args) throws Exception {
 	BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 	int n = Integer.parseInt(in.readLine().trim());
